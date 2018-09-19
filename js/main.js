f195760a5cb889438fcb6b929f77d2b60fcc4d24
tylerstephens814 <tylerstephens814@gmail.com>
2018-09-19 14:22:20 -0600

    Display list elements in the html

--- js/main.js
+++ js/main.js
@@ -1,9 +1,27 @@
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+}
+
 class TodoItem {
   constructor(title){
     this.title = title;
     this.created = Date.now()
     this.completed = false;
+    this.id = guid()
   }
+}
+
+function updateView(){
+  var list = $('#list')
+  list.html('')
+  items.forEach((x)=> {
+    list.append('<div class="todo-item">'+ x.title + '</div>')
+  })
 }
 
 var items = []
@@ -12,16 +30,11 @@ function addNew(){
   var title = $('#title').val()
   if (title.length) {
     items.push(new TodoItem(title))
-    console.log(items)
-    clearField();
+    $('#title').val('')
     $('#title').focus()
+    updateView()
   }
 }
-
-function clearField(){
-  $('#title').val('')
-}
-
 
 $( document ).ready(function() {
   $("#submit").click(function(){
@@ -33,3 +46,4 @@ $( document ).ready(function() {
     }
   })
 });
+
